d181911a7cc3e84d6f4549f420fda3eec7758783
JeongHwanjun <ghkswnsdlwjd@naver.com>
2024-11-15 12:26:07 +0900

    get_stock_price inital commit

--- stock_data_provider.py
+++ stock_data_provider.py
@@ -1,5 +1,11 @@
+from pykrx import stock
+from datetime import date
+
 def get_stock_price(stock_code: str) -> float:
-    return 0
+    today = date.today()
+    todayStr = today.strftime("%Y%m%d")
+    df = stock.get_market_ohlcv(todayStr, todayStr, stock_code)
+    return df.iloc[0]['종가']
 
 def is_buyable_price(ticker_code: str, indic_name: str) -> int:
     #1: buy signal, 0, 유보 -1: Sell signal
